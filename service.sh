7b551972b5ae5589174efcea185b58337c373a6b
Saurabh Raman <saurcode@gmail.com>
2025-12-11 20:18:58 +0200

    added neodash:

--- service.sh
+++ service.sh
@@ -10,7 +10,7 @@ kubectl port-forward -n openstudybuilder svc/consumerapi 5008:5008 >/dev/null 2>
 kubectl port-forward -n openstudybuilder svc/documentation 5006:5006 >/dev/null 2>&1 &
 kubectl port-forward -n openstudybuilder svc/neodash 5007:5007 >/dev/null 2>&1 &
 kubectl port-forward -n openstudybuilder svc/pocketbase 8090:8090 >/dev/null 2>&1 &
-kubectl port-forward -n openstudybuilder svc/database 7474:7474 7687:7687 >/dev/null 2>&1 &
+kubectl port-forward -n openstudybuilder svc/database 7474:7474 7687:7687 5002:7687 >/dev/null 2>&1 &
 
 echo "--- All services exposed on localhost! ---"
 echo "Frontend:      http://localhost:5005"
@@ -18,6 +18,6 @@ echo "API:           http://localhost:5003"
 echo "Consumer API:  http://localhost:5008"
 echo "Docs:          http://localhost:5006"
 echo "Neo4j Browser: http://localhost:7474"
-echo "Neo4j Bolt:    bolt://localhost:7687"
+echo "Neo4j Bolt:    bolt://localhost:7687 (or :5002)"
 echo "PocketBase:    http://localhost:8090"
 echo "Neodash:       http://localhost:5007"
